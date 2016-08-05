b7a58f9a5b7f1ab9d9daf750daa22b7a17b2ae1b
Tobias Helmrich <tobi@tobiass-macbook-pro.local>
2016-08-05 14:53:44 +0200

    Create outlets and action, implement updateColors function

--- Color Combinator/ViewController.swift
+++ Color Combinator/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateColors() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        // Update the slider's minimum track tint color (left to the thumbs position)
+        // to match the color's current value
+        redSlider.minimumTrackTintColor = UIColor(red: red / 255, green: 0, blue: 0, alpha: 1)
+        greenSlider.minimumTrackTintColor = UIColor(red: 0, green: green / 255, blue: 0, alpha: 1)
+        blueSlider.minimumTrackTintColor = UIColor(red: 0, green: 0, blue: blue / 255, alpha: 1)
+        
+        colorView.backgroundColor = UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1.0)
+        
+    }
+    
+    
 
 }
 
